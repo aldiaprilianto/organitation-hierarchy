48d008644fab1f97e021e62adbc8c3e1bfa500ff
Aldi Aprilianto <aldi.aprilianto@btpnsyariah.com>
2024-03-23 14:39:13 +0700

    fixing CORS

--- main.go
+++ main.go
@@ -3,10 +3,9 @@ package main
 import (
 	"github.com/aldiaprilianto/takana/config"
 	"github.com/aldiaprilianto/takana/router"
-	"github.com/aldiaprilianto/takana/utility"
-
 	"github.com/gin-gonic/gin"
 	"github.com/sirupsen/logrus"
+	"net/http"
 )
 
 func main() {
@@ -31,7 +30,19 @@ func main() {
 	r := gin.Default()
 
 	/* Middleware CORS */
-	r.Use(utility.CORS())
+	//r.Use(utility.CORS())
+	// Middleware untuk menangani CORS
+	r.Use(func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, OPTIONS")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "*")
+		c.Writer.Header().Set("Access-Control-Max-Age", "86400")
+		if c.Request.Method == http.MethodOptions {
+			c.AbortWithStatus(http.StatusNoContent)
+			return
+		}
+		c.Next()
+	})
 
 	/* Routes */
 	routing.SetupRoutes(r)
